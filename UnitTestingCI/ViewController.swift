cf6869c9cea3e6d8652f2e6f386ed88eb72f944b
leandro <leandro.estrada@itsgroup.com.br>
2020-06-09 20:55:05 -0300

    Initial Commit

---
+++ UnitTestingCI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UnitTestingCI
+//
+//  Created by leandro de araujo estrada on 09/06/20.
+//  Copyright © 2020 leandro de araujo estrada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ UnitTestingCITests/UnitTestingCITests.swift
@@ -0,0 +1,34 @@
+//
+//  UnitTestingCITests.swift
+//  UnitTestingCITests
+//
+//  Created by leandro de araujo estrada on 09/06/20.
+//  Copyright © 2020 leandro de araujo estrada. All rights reserved.
+//
+
+import XCTest
+@testable import UnitTestingCI
+
+class UnitTestingCITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
